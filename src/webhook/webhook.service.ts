4dfc6ff497c1e77f0ff0cd3bd1a64f5646c7e179
YuriAPCarvalho <yuri.apcarvalho@gmail.com>
2025-12-05 15:01:03 -0400

    Filtrar json

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -9,11 +9,21 @@ export class WebhookService {
   ) {}
 
   async save(payload: any) {
-    const doc = new this.webhookModel({ body: payload });
+    const filteredPayload = {
+      id: payload.resource?.id,
+      tipo: payload.resource?.fields?.['System.WorkItemType'],
+      titulo: payload.resource?.fields?.['System.Title'],
+      sprint: payload.resource?.fields?.['System.IterationPath'],
+      criadoEm: payload.resource?.fields?.['System.CreatedDate'],
+      atualizadoEm: payload.resource?.fields?.['System.ChangedDate'],
+      status: payload.resource?.fields?.['System.State'],
+    };
+
+    const doc = new this.webhookModel(filteredPayload);
     return doc.save();
   }
 
   async findAll() {
-    return this.webhookModel.find().sort({ timestamp: -1 }).exec();
+    return this.webhookModel.find().sort({ criadoEm: -1 }).exec();
   }
 }
